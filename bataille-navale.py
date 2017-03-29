8feca7d266ee3b98c2f0d773915a5e48811b1086
Mathieu Nachury <mathieu.nachury@outlook.com>
2017-03-29 15:55:53 +0200

    code grille 1.1

--- bataille-navale.py
+++ bataille-navale.py
@@ -8,7 +8,7 @@ class  batailleNavale():
     def __init__(self):
         pass
     
-    #Intialise une grille de taille x par y remplie de 0
+    #Intialise une grille de taille x par y remplie de 0 test2
     def  createGrille(self, x, y):
         self._grille = [[0] * x for _ in range(y)]
     
