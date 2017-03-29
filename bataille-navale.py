c8446920fdf0ad63ae48e9edd1dc7b7be9b76157
Mathieu Nachury <mathieu.nachury@outlook.com>
2017-03-29 15:10:30 +0200

    base

--- bataille-navale.py
+++ bataille-navale.py
@@ -1,1 +1,11 @@
-import
+import unittest
+
+class TestBn(unittest.TestCase):
+    def testbnBase(self):
+        Bn = batailleNavale()
+        
+class  batailleNavale():
+    def __init__(self):
+        pass
+
+unittest.main()
