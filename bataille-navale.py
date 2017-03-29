59e2efb48b662b5abae87bbe237e1d7ed1f36eec
Mathieu Nachury <mathieu.nachury@outlook.com>
2017-03-29 15:53:30 +0200

    code grille 1

--- bataille-navale.py
+++ bataille-navale.py
@@ -7,5 +7,12 @@ class TestBn(unittest.TestCase):
 class  batailleNavale():
     def __init__(self):
         pass
+    
+    #Intialise une grille de taille x par y remplie de 0
+    def  createGrille(self, x, y):
+        self._grille = [[0] * x for _ in range(y)]
+    
+    def grille(self):
+        return self._grille
 
 unittest.main()
