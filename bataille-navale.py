409b1f2da25b74021bce6da80ede1aca3884742e
Mathieu Nachury <mathieu.nachury@outlook.com>
2017-03-30 14:33:17 +0200

    add code pour erreur taille bateaux, modification constructeur pour création grille suppression d'un test

--- bataille-navale.py
+++ bataille-navale.py
@@ -9,13 +9,12 @@ minY = 5
 class TestBn(unittest.TestCase):
     # Test de base, instanciation classe
     def test_bnBase(self):
-        bn = batailleNavale()
+        bn = batailleNavale(12,12)
         self.assertIsNot(None, bn)
 
     # Test d'initialisation d'une grille vide
     def test_CreateGrille(self):
-        bn = batailleNavale()
-        bn.createGrille(10, 50)
+        bn = batailleNavale(10, 50)
         grille = bn.grille()
         self.assertEqual(50, len(grille))
         for row in grille:
@@ -25,8 +24,7 @@ class TestBn(unittest.TestCase):
 
     # Test grille trop grande (maximum maxX;maxY)
     def test_TooBigGrille(self):
-        bn = batailleNavale()
-        bn.createGrille(500, 101)
+        bn = batailleNavale(500, 101)
         grille = bn.grille()
         self.assertNotEqual(101, len(grille))
         self.assertEqual(maxY, len(grille))
@@ -38,8 +36,7 @@ class TestBn(unittest.TestCase):
 
     # Test grille trop petite (minimum minX;minY)
     def test_TooSmallGrille(self):
-        bn = batailleNavale()
-        bn.createGrille(3, 2)
+        bn = batailleNavale(3, 2)
         grille = bn.grille()
         self.assertNotEqual(2, len(grille))
         self.assertEqual(minY, len(grille))
@@ -51,8 +48,7 @@ class TestBn(unittest.TestCase):
 
     # Test grille négative
     def test_NegativeGrille(self):
-        bn = batailleNavale()
-        bn.createGrille(-30, -12000)
+        bn = batailleNavale(-30, -12000)
         grille = bn.grille()
         self.assertNotEqual(-12000, len(grille))
         self.assertEqual(minY, len(grille))
@@ -62,7 +58,7 @@ class TestBn(unittest.TestCase):
 
     # Test initialisation type bateau
     def test_CreateTypeBateau(self):
-        bn = batailleNavale()
+        bn = batailleNavale(10, 10)
         i = bn.createTypeBateau(5, 1)
         typeBateau = bn.typeBateau(i)
         self.assertEqual(5, typeBateau[0])
@@ -70,7 +66,7 @@ class TestBn(unittest.TestCase):
 
     # Test initialisation bateau
     def test_CreateBateau(self):
-        bn = batailleNavale()
+        bn = batailleNavale(10, 10)
         i = bn.createTypeBateau(1, 4)
         idBateau = bn.createBateau(0, 0, i)
         bateau = bn.bateau(idBateau)
@@ -80,40 +76,30 @@ class TestBn(unittest.TestCase):
 
     # Test bateau trop grand
     def test_TooBigBateau(self):
-        bn = batailleNavale()
-        bn.createGrille(10, 10)
+        bn = batailleNavale(10, 10)
         i = bn.createTypeBateau(11, 6)
-        typeBateau = bn.typeBateau(i)
-        self.assertIsNone(typeBateau)
+        self.assertIsNone(i)
 
     # Test bateau avec dimensions négatives
     def test_NegativeBateau(self):
-        bn = batailleNavale()
+        bn = batailleNavale(10, 10)
         i = bn.createTypeBateau(-1000, -12)
-        typeBateau = bn.typeBateau(i)
-        self.assertIsNone(typeBateau)
+        self.assertIsNone(i)
 
-    # Test bateaux invalide (positionnement impossible)
-    def test_TooBigBateaux(self):
-        bn = batailleNavale()
-        bn.createGrille(10, 10)
-        i = bn.createTypeBateau(8, 6)
-        typeBateau = bn.typeBateau(i)
-        self.assertIsNotNone(typeBateau)
-        i = bn.createTypeBateau(8, 6)
-        typeBateau = bn.typeBateau(i)
-        self.assertIsNone(typeBateau)
+    # Test types bateaux invalide (positionnement impossible)
+    # test enlevé car l'erreur est au niveau de la creation des bateaux
 
 
 class batailleNavale():
-    def __init__(self):
+    def __init__(self, x, y):
+        self._createGrille(x, y)
         self._bateaux = []
         self._typeBateaux = []
 
     # Fonction grille
 
     # Intialise une grille de taille x par y remplie de 0
-    def createGrille(self, x, y):
+    def _createGrille(self, x, y):
         if x > maxX: x = maxX
         if y > maxY: y = maxY
         if x < minX: x = minX
@@ -126,6 +112,8 @@ class batailleNavale():
     # Fonction bateaux
 
     def createTypeBateau(self, x, y):
+        if x > len(self._grille) or y > len(self._grille[0]) or x < 0 or y < 0:
+            return None
         self._typeBateaux.append([x, y])
         # return 1 pour 1 bateau (même si id 0)
         return len(self._typeBateaux)
